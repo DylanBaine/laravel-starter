40abe4e1135be0fe7e6819bd81b728bcc28b43c6
Dylan Baine <dylan_baine@sweetwater.com>
2023-12-07 11:22:04 -0500

    init make:feature

---
+++ app/Console/Commands/MakeFeatureCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeFeatureCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:feature {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
