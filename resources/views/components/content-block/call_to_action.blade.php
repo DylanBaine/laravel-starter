1014fb2aab6a9fe09e27f43793113fbcbaba1075
Dylan Baine <dylan_baine@sweetwater.com>
2024-01-20 15:58:55 -0500

    fill icon

--- resources/views/components/content-block/call_to_action.blade.php
+++ resources/views/components/content-block/call_to_action.blade.php
@@ -1,6 +1,7 @@
 @props(['data'])
 <div class="text-center">
     <button class="btn {{ isset($data['size']) ? $data['size'] : 'btn-lg' }} btn-primary">
-        <span>{{ $data['text'] ?? 'Get Started' }}</span> <x-icons.chevron-left class="rotate-180 w-[2em]" />
+        <span>{{ $data['text'] ?? 'Get Started' }}</span> <x-icons.chevron-left
+            class="rotate-180 w-[2em] fill-base-100" />
     </button>
 </div>
