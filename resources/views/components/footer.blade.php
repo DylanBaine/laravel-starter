bd527e623b43758ed74e928d543986675f7cc5fa
Dylan Baine <dylan_baine@sweetwater.com>
2024-01-20 12:02:35 -0500

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
 <footer class="mt-4">
-    <div class="p-2 bg-base-200 min-h-[8em] flex items-center">
-        <div>
+    <div class="p-2 bg-base-200 min-h-[8em] flex items-center min-h-[40vh]">
+        <div class="max-w-7xl mx-auto w-full">
             <span class="font-bold brightness-150">
                 Footer
             </span>
         </div>
     </div>
     <div class="p-2 bg-base-300">
-        <div>
+        <div class="max-w-7xl mx-auto">
             &copy; - {{ date('Y') }}
         </div>
     </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header class="px-2 py-4 bg-base-200 sticky top-0 z-50">
     <nav class="max-w-6xl mx-auto flex justify-between items-center h-full">
         <div class="flex items-center h-full">
-            <span class="brightness-150 font-bold">Header</span>
+            <a href="/" class="brightness-150 font-bold">Header</a>
         </div>
         <div class="text-right flex gap-4 items-center h-full">
             <a href="/blog" class="font-bold text-lg">Blog</a>
