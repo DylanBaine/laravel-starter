dab44a8c7e3a60bb8f7a94a4c5d7b0341f522e0b
Dylan Baine <dylan_baine@sweetwater.com>
2024-01-20 15:24:48 -0500

    underline

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="p-2 bg-base-200 min-h-[8em] flex items-center min-h-[40vh]">
         <div class="max-w-7xl mx-auto w-full">
             <span class="font-bold brightness-150">
-                a product of <a href="https://infactodigital.com?utm_source=infacto.digital+footer">InfactoDigital</a>
+                a product of <a class="underline"
+                    href="https://infactodigital.com?utm_source=infacto.digital+footer">InfactoDigital</a>
             </span>
         </div>
     </div>
